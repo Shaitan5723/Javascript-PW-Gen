145378dabb0aac1864239c14c785fc6e5561defc
Shaitan5723 <105050107+Shaitan5723@users.noreply.github.com>
2022-06-02 11:50:07 +1000

    variables and functions
    Established main variables for password generator.
    Function for getting random variables written

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-// Assignment code here
+var upperLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var lowerLetters = 'abcdefghijklmnopqrstuvwxyz';
+var numbers = '0123456789';
+var symbols = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 
+
+function getLowercase () {
+  return lowerLetters[Math.floor(Math.random() * lowerLetters.length)];
+}
+function getUppercase () {
+  return upperLetters[Math.floor(Math.random() * upperLetters.length)];
+}
+function getNumber () {
+  return numbers[Math.floor(Math.random() * numbers.length)];
+}
+function getSymbol () {
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -15,3 +31,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// console.log (a) -
